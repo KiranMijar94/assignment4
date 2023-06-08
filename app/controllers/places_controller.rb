990d68f9f3c03fba030ef0e468afc3d009371c8f
Kiran Mijar <kiran.mijar@kellogg.northwestern.edu>
2023-06-08 04:06:23 +0000

    Final Assignment Submission

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,11 +2,12 @@ class PlacesController < ApplicationController
 
   def index
     @places = Place.all
+    @current_user = User.find_by({ "id" => session["user_id"] })
   end
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @posts = Post.where({ "place_id" => @place["id"] })
+    @posts = Post.where({ "place_id" => @place["id"], "user_id" => session["user_id"] })
   end
 
   def new
@@ -14,10 +15,24 @@ class PlacesController < ApplicationController
   end
 
   def create
-    @place = Place.new
-    @place["name"] = params["place"]["name"]
-    @place.save
+    @place = Place.new(place_params)
+    @place.created_at = Time.now
+    @place.updated_at = Time.now
+   if @place.save
     redirect_to "/places"
+   else
+      @places = Place.all
+      render :action => 'new'
+   end
+  end
+
+  def delete
+    Place.find(params[:name]).destroy
+    redirect_to :action => 'index'
+  end
+
+  def place_params
+    params.require(:place).permit(:name)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,32 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new
-    @user["first_name"] = params["user"]["first_name"]
-    @user["last_name"] = params["user"]["last_name"]
-    @user["email"] = params["user"]["email"]
-    @user["password"] = params["user"]["password"]
-    @user.save
+    @user = User.new(user_params)
+    @user.password = BCrypt::Password.create(params["user"]["password"])
+    @user.created_at = Time.now
+    @user.updated_at = Time.now
+   if @user.save
     redirect_to "/"
+   else
+      @users = User.all
+      render :action => 'new'
+   end
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def delete
+    User.find(params[:username]).destroy
+    redirect_to :action => 'index'
+  end
+
+  def show
+    @user = User.find_by(:username => params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
